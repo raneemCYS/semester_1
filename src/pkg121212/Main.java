a258bd5f2b7523fcda47961a43d52b01add3fae4
raneemCYS <itsrain333330@gmail.com>
2026-01-07 20:52:04 +0300

    Add files via upload

---
+++ src/pkg121212/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg121212;
+
+/**
+ *
+ * @author ranwi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        newnew book=new newnew();
+        
+        book.setauthor("raneem");
+        book.setpageCount(333);
+        book.settitle("the");
+        System.out.println("the book author:"+book.getauthor());
+        System.out.println("the book name:"+book.gettitle());
+        System.out.println("the book page numbers:"+book.getpageCount());
+        
+        
+    }
+    
+    
+}
---
+++ src/pkg121212/newnew.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg121212;
+
+/**
+ *
+ * @author ranwi
+ */
+public class newnew {
+    private String title;
+    private String author;
+    private int pageCount; 
+    
+    
+   
+   public void settitle(String s){
+    title=s; }
+   public void setauthor(String s){
+    author=s; }
+   public void setpageCount(int s){
+    pageCount=s; }
+   
+   public String gettitle(){
+   return title;}
+   public String getauthor(){
+   return author;}
+   public int getpageCount(){
+   return pageCount;}
+   
+}
